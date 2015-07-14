43da8a6d55bbdf66922330d98a375392d011133e
Wilco Fiers <wilco@pangeadc.nl>
2015-07-14 17:48:29 +0200

    comgined client/server into earlReporter script

--- public/scripts/earlReporter.js
+++ public/scripts/earlReporter.js
@@ -5,11 +5,176 @@
     var _browser = !_nodejs &&
         (typeof window !== 'undefined' || typeof self !== 'undefined');
 
-    if (_nodejs) {
 
-    } else if (_browser) {
+    // Get external dependencies
+    var Handlebars   = (_nodejs ? require('handlebars') : window.Handlebars);
+    var jsonld       = (_nodejs ? require('./jsonld') : window.jsonld);
+    var context      = (_nodejs ? require('./context') : window.context);
+    var wr20specData = (_nodejs ? require('./wr20specdata') : window.wr20specData);
 
-    }
+
+    // search/replace in string
+    // this might be an alternative: http://formatjs.io/handlebars/ - but we need to upgrade node to something higher than version 0.10
+    Handlebars.registerHelper('dutchDate', function(input) {
+      var months = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december'];
+      var parts = Handlebars.escapeExpression(input).split('-');
+      var output = '';
+
+      if(parts.length === 3) {
+        output = parseInt(parts[2], 10)+' '+months[parseInt(parts[1], 10) - 1]+' '+parts[0];
+      }
+
+      return new Handlebars.SafeString(
+        output
+      );
+    });
+
+    // create link from string
+    Handlebars.registerHelper('link', function(string) {
+      var url = Handlebars.escapeExpression(string);
+
+      return new Handlebars.SafeString(
+        "<a href='" + url + "'>" + url + "</a>"
+      );
+    });
+
+    // turn string into list
+    Handlebars.registerHelper('list', function(string) {
+      string = Handlebars.escapeExpression(string);
+      var items = string.split("\n");
+      var list = '';
+
+      items.forEach(function(item) {
+        list += '<li>'+item+'</li>';
+      });
+
+      return new Handlebars.SafeString(list);
+    });
+
+    // add lengths of arrays together
+    Handlebars.registerHelper('count', function() {
+      var i;
+      var count = 0;
+
+      for (i = 0; i < arguments.length - 1; i += 1) {
+        if(Array.isArray(arguments[i])) {
+          count += arguments[i].length;
+        }
+      }
+
+      return count;
+    });
+
+    // parse string to HTML (add links, paragraphs and break elements)
+    Handlebars.registerHelper('html', function(string) {
+      string = Handlebars.escapeExpression(string);
+      var tmp;
+      var html = '';
+
+      // add links
+      tmp = [];
+      string.split(' ').forEach(function(word) {
+        if(word.indexOf('www.') === 0) {
+          word = 'http://'+word;
+        }
+
+        if(word.indexOf('http://') === 0 || word.indexOf('https://') === 0) {
+          word = '<a href="'+word+'">'+word+'</a>';
+        }
+
+        tmp.push(word);
+      });
+      string = tmp.join(' ');
+
+      // create paragraphs
+      string.split("\n\n").forEach(function(paragraph) {
+        html += '<p>'+paragraph+'</p>';
+      });
+
+      // create line breaks
+      html.replace(
+        SearchRegEx = new RegExp(
+          "\n".replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1"), 'g'
+        ), '<br/>'
+      );
+      return new Handlebars.SafeString(html);
+    });
+
+
+    /**
+     * Usage: earlReporter.createReport(templateStr, data, callback(error, html)
+     *
+     * @type {Object}
+     */
+    var earlReporter = {
+        createReport: function(templateString, jsonSource, callback) {
+            jsonld.compact(jsonSource, context, function (error, evalData) {
+                var output;
+                if (!error) {
+                    var templContent = earlReporter.getTemplateContent(evalData);
+                    var template     = Handlebars.compile(templateString);
+                    output           = template(templContent);
+                }
+                callback(error, output);
+            });
+        },
+
+        getTemplateContent: function (jsonld) {
+            var templContent = {
+                eval: null,
+                persons: []
+            };
+            jsonld['@graph'].forEach(function (obj) {
+                if (obj.type === 'evaluation') {
+                    templContent.eval = obj;
+                } else {
+                    templContent.persons.push(obj);
+                }
+            });
+
+            var critResult = earlReporter.buildCritResults(
+                    templContent.eval.auditResult);
+
+            templContent.principles = earlReporter.buildSpec(critResult, wr20specData);
+
+            return templContent;
+        },
+
+        buildCritResults: function (auditResult) {
+            return auditResult.reduce(function (critResults, assert) {
+                if (assert.hasPart.length > 0 ||
+                    assert.result.outcome !== 'earl:untested' ||
+                    assert.result.description) {
+
+                    critResults[assert.testRequirement] = assert;
+                }
+                return critResults;
+            }, {});
+        },
+
+        buildSpec: function (critResults, principles) {
+            // Give a hasResult property to all p/c/g & assign the assertions
+            principles.forEach(function (principle) {
+                principle.hasResult = principle.guidelines
+                .reduce(function (hasResult, guideline) {
+
+                    guideline.hasResult = guideline.criteria
+                    .reduce(function (hasResult, criterion) {
+
+                        criterion.hasResult = !!critResults[criterion.uri];
+                        if (criterion.hasResult) {
+                            criterion.assertion = critResults[criterion.uri];
+                        }
+                        return hasResult || criterion.hasResult;
+                    }, false);
+
+                    return hasResult || guideline.hasResult;
+                }, false);
+            });
+            return principles;
+        }
+    };
+
 
     if(_nodejs) {
         // export nodejs API
@@ -22,4 +187,5 @@
     } else if(_browser) {
         window.earlReporter = earlReporter;
     }
+
 }());
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 var express      = require('express');
-var jsonld       = require('../public/scripts/jsonld');
-var context      = require('../public/scripts/context');
-var wr20specData = require('../public/scripts/wr20specdata');
-var handlebars   = require('handlebars');
+var earlReporter = require('../public/scripts/earlReporter');
 var fs           = require('fs');
 var router       = express.Router();
 
@@ -11,74 +8,57 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
-function buildSpec(auditResult, principles) {
-    // Create a map with the URI of all none empty assertions
-    var critResults = auditResult
-    .reduce(function (critResults, assert) {
-        if (assert.hasPart.length > 0 ||
-            assert.result.outcome !== 'earl:untested' ||
-            assert.result.description) {
-
-            critResults[assert.testRequirement] = assert;
-        }
-        return critResults;
-    }, {});
-
-    // Give a hasResult property to all p/c/g & assign the assertions
-    principles.forEach(function (principle) {
-        principle.hasResult = principle.guidelines
-        .reduce(function (hasResult, guideline) {
-
-            guideline.hasResult = guideline.criteria
-            .reduce(function (hasResult, criterion) {
-
-                criterion.hasResult = !!critResults[criterion.uri];
-                if (criterion.hasResult) {
-                    criterion.assertion = critResults[criterion.uri];
-                }
-                return hasResult || criterion.hasResult;
-            }, false);
-
-            return hasResult || guideline.hasResult;
-        }, false);
-    });
-
-    return principles;
-}
-
-
 router.post('/build-report', function(req, res) {
 	var tplPath = 'public/reports/default/main.hds';
 
+    // read the file and use the callback to render
+    fs.readFile(tplPath, function(err, templateFile) {
+        if (err) {
+            res.send(err);
+            return;
+        }
+        var jsonSource     = req.body;
+        var templateString = templateFile.toString();
 
-	jsonld.compact(req.body, context, function (err, json) {
-        var templContent = {
-            eval: null,
-            persons: []
-        };
-
-        json['@graph'].forEach(function (obj) {
-            if (obj.type === 'evaluation') {
-                templContent.eval = obj;
+        earlReporter.createReport(templateString, jsonSource, function (err, htmlReport) {
+            if (htmlReport) {
+                res.send(htmlReport);
             } else {
-                templContent.persons.push(obj);
+                res.send(err);
             }
         });
-
-        templContent.principles = buildSpec(templContent.eval.auditResult,
-                                                        wr20specData);
-
-        // read the file and use the callback to render
-		fs.readFile(tplPath, function(err, templateFile) {
-			if (err) {
-				res.send(err);
-				return;
-		    }
-		    // make the buffer into a string, then to a template
-		    var template = handlebars.compile(templateFile.toString());
-			res.send(template(templContent));
-		});
     });
+
+
+
+	// jsonld.compact(req.body, context, function (err, json) {
+ //        var templContent = {
+ //            eval: null,
+ //            persons: []
+ //        };
+
+ //        json['@graph'].forEach(function (obj) {
+ //            if (obj.type === 'evaluation') {
+ //                templContent.eval = obj;
+ //            } else {
+ //                templContent.persons.push(obj);
+ //            }
+ //        });
+
+ //        templContent.principles = buildSpec(templContent.eval.auditResult,
+ //                                                        wr20specData);
+
+ //        // read the file and use the callback to render
+	// 	fs.readFile(tplPath, function(err, templateFile) {
+	// 		if (err) {
+	// 			res.send(err);
+	// 			return;
+	// 	    }
+	// 	    // make the buffer into a string, then to a template
+	// 	    var template = handlebars.compile(templateFile.toString());
+	// 		res.send(template(templContent));
+	// 	});
+ //    });
 
 
 });
